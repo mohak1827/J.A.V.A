8f8f1b5a51e730985d1ebbba8e009f106c3a7656
mohak1827 <mohak1827vk@gmail.com>
2026-07-03 04:29:04 +0530

    ternary operator

--- src/conditional_statements.java
+++ src/conditional_statements.java
@@ -1,41 +1,89 @@
+//public class conditional_statements {
+//
+//    public static void main(String args[]){
+//
+//        // if condition
+//
+//        int x = 5;
+//
+//        if(x < 10){
+//            System.out.println("Condition is true");
+//        }
+//
+//        // if-else statements
+//
+//        int a = 3;
+//        int b = 7;
+//
+//        if(a > b){
+//            System.out.println("a is greater");
+//        }else{
+//            System.out.println("b is greater");
+//        }
+//
+//        // if - else if- else statements
+//
+//        int num1 = 5;
+//        int num2 = 7;
+//        int num3 = 9;
+//
+//        if(num1 > num2 && num1 > num3){
+//            System.out.println("num1 is greatest among 3");
+//        }
+//        else if(num2 > num3){
+//            System.out.println("num2 is greatest among 3");
+//        }
+//        else{
+//            System.out.println("num3 is greatest among 3");
+//        }
+//
+//    }
+//}
+
+
+
+
+// ternary operator
+
+//public class conditional_statements {
+//
+//    public static void main(String args[]){
+//
+//        int num = 4;
+//
+//        boolean eve = num%2 == 0 ? true : false;
+//
+//        if(eve){
+//            System.out.println("It is an even number");
+//        }
+//        else{
+//            System.out.println("It is an odd number");
+//        }
+//
+//        // nested ternary
+//
+//        int marks = 77;
+//
+//        String grade =
+//                (marks > 80) ? "O" :
+//                (marks > 70) ? "A+" :
+//                (marks > 60) ? "A" :
+//                (marks > 50) ? "B+" :
+//                (marks > 40) ? "P" : "F";
+//
+//        System.out.println(grade);
+//    }
+//}
+
+
+
+
+// switch statements
+
 public class conditional_statements {
 
     public static void main(String args[]){
 
-        // if condition
-
-        int x = 5;
-
-        if(x < 10){
-            System.out.println("Condition is true");
-        }
-
-        // if-else statements
-
-        int a = 3;
-        int b = 7;
-
-        if(a > b){
-            System.out.println("a is greater");
-        }else{
-            System.out.println("b is greater");
-        }
-
-        // if - else if- else statements
-
-        int num1 = 5;
-        int num2 = 7;
-        int num3 = 9;
-
-        if(num1 > num2 && num1 > num3){
-            System.out.println("num1 is greatest among 3");
-        }
-        else if(num2 > num3){
-            System.out.println("num2 is greatest among 3");
-        }
-        else{
-            System.out.println("num3 is greatest among 3");
-        }
 
     }
 }

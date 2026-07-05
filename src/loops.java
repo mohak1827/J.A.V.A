f51dfe406103ba5e393e158fdef9ea7ad4679467
mohak1827 <mohak1827vk@gmail.com>
2026-07-05 17:17:54 +0530

    for and nested for loops

--- src/loops.java
+++ src/loops.java
@@ -14,7 +14,7 @@ public class loops {
 //        System.out.println("Bye " + i);
 
 
-//        // nested while loop
+        // nested while loop
 //
 //        int i = 1;
 //
@@ -37,16 +37,49 @@ public class loops {
 
 
         // do-while loop
+//
+//        int i = 5;
+//
+//        do  // will execute the block atleast once, (first executes the block of code, then checks the condition)
+//        {
+//
+//            System.out.println("Hi " + i); // will print "5" and then increment i and then will check the condition
+//            i++;
+//
+//        } while(i <= 4);
 
-        int i = 5;
 
-        do  // will execute the block atleast once, (first executes the block of code, then checks the condition)
+        // for loop
+
+        //  In while and do-while loop we are initializing the variable, incrementing, and checking the condition in three different lines, but in the for loop we can make it in one single line when we know the number of times we have to execute the block of code
+
+//        for(int i = 1; i <= 4; i++)
+//        {
+//            System.out.println("Hi " + i);
+//        }
+
+//        int i = 1;
+//
+//        for(;i <= 4;) // we can also write a for loop like this, but the semicolons are necessary.
+//        {
+//            System.out.println("Hi " + i);
+//            i++;
+//        }
+
+        // nested for loop
+
+        for(int i = 1; i <= 4; i++)
         {
+            System.out.println("Hi, i = " + i);
 
-            System.out.println("Hi " + i); // will print "5" and then increment i and then will check the condition
-            i++;
+            for(int j = 1; j <=3; j++)
+            {
+                System.out.println("j = " + j);
+                System.out.println("i + j = " + (i+j) + " ");
+            }
 
-        } while(i <= 4);
+            System.out.println();
 
+        }
     }
 }

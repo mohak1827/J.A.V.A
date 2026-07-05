79b8e2986723910e54b05a3f63b5984bfef4139e
mohak1827 <mohak1827vk@gmail.com>
2026-07-05 16:09:28 +0530

    switch case statement

--- src/conditional_statements.java
+++ src/conditional_statements.java
@@ -84,6 +84,32 @@ public class conditional_statements {
 
     public static void main(String args[]){
 
+        int day = 7;
 
+        switch(day){
+            case 1 :
+                System.out.println("Monday");
+                break; // necessary because if switch found a match then it will not check further cases to be matched it will execute all of them without checking
+            case 2 :
+                System.out.println("Tuesday");
+                break;
+            case 3 :
+                System.out.println("Wednesday");
+                break;
+            case 4 :
+                System.out.println("Thursday");
+                break;
+            case 5 :
+                System.out.println("Friday");
+                break;
+            case 6 :
+                System.out.println("Saturday");
+                break;
+            case 7 :
+                System.out.println("Sunday");
+                break;
+            default :
+                System.out.println("Enter a valid number");
+        }
     }
 }

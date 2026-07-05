359c8bbe041a43670c3469a5325432b26d1eecaf
mohak1827 <mohak1827vk@gmail.com>
2026-07-05 16:59:09 +0530

    do-while loop

--- src/loops.java
+++ src/loops.java
@@ -13,26 +13,40 @@ public class loops {
 //
 //        System.out.println("Bye " + i);
 
-        // nested while loop
 
-        int i = 1;
+//        // nested while loop
+//
+//        int i = 1;
+//
+//        while(i <= 4)   // will execute the block of code until the condition becomes false
+//        {
+//            System.out.println("Hi " + i);
+//
+//            int j = 1;
+//                            // for each time the upper condition is true, it will again make a variable j and reassign the value to it and will execute the below block till the condition is true
+//            while(j <= 3)
+//            {
+//                System.out.println("Hello " + j);
+//                j++;
+//            }
+//
+//            i++;
+//        }
+//
+//        System.out.println("Bye " + i);
 
-        while(i <= 4)
+
+        // do-while loop
+
+        int i = 5;
+
+        do  // will execute the block atleast once, (first executes the block of code, then checks the condition)
         {
-            System.out.println("Hi " + i);
 
-            int j = 1;
-
-            while(j <= 3)
-            {
-                System.out.println("Hello " + j);
-                j++;
-            }
-
+            System.out.println("Hi " + i); // will print "5" and then increment i and then will check the condition
             i++;
-        }
 
-        System.out.println("Bye " + i);
+        } while(i <= 4);
 
     }
 }

9e1bf6638bc8c47be9086d67f567c7fa9cce1001
mohak1827 <mohak1827vk@gmail.com>
2026-07-22 01:08:45 +0530

    inner class concepts

--- src/innerClass/anonymousInnerClass.java
+++ src/innerClass/anonymousInnerClass.java
@@ -11,6 +11,7 @@ public class anonymousInnerClass
     public static void main(String[] args)
     {
         X obj = new X()     // if we want some different implementation, we can directly implement here instead of creating a inherited class for only a specific functionality
+        // anonymous inner class
         {
             public void show()
             {
